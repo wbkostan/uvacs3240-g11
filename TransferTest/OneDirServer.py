550669b613c3772b68b531dd9027d5db9b0171d8
jha5cn <jha5cn@virginia.edu>
2013-11-24 17:53:10 -0500

    Command line

--- TransferTest/OneDirServer.py
+++ TransferTest/OneDirServer.py
@@ -4,8 +4,70 @@ from Controllers.Server.Controller import ServerController
 import time
 import sys
 
-def __init__(self):
-    self.controller = ServerController()
+class OneDirServer():
+    def __init__(self):
+        self.controller = ServerController()
+
+    #Creates account
+    def create_account(self):
+        print("Enter user name")
+        user_name = raw_input()
+        print("Enter password")
+        password = raw_input()
+    #######
+        #Code goes here
+    #######
+
+    #Turns automatic syncing on
+    def sync(self):
+        config = get_config()
+        setup_django()
+        self.controller.configure(config)
+        self.controller.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            self.controller.__teardown__()
+
+    #User command that changes signed on user's password
+    def change_pass(self):
+        print("Nothing yet")
+    #######
+        #Code goes here
+    #######
+
+    #Prints user database
+    def all_users(self):
+        print("Nothing yet")
+    #######
+        #Code goes here
+    #######
+
+    #Prints files associated with a user
+    def print_user_files(self):
+        print("Enter username:")
+        username = raw_input()
+        self.controller.print_user_files(username)
+
+    #Removes user
+    def remove(self):
+        print("Enter username:")
+        username = raw_input()
+    #######
+        #Code goes here
+    #######
+
+    #Admin command that changes the password of the given user
+    def change_user_pass(self):
+        print("Enter username:")
+        username = raw_input()
+
+    def history(self):
+        print("Nothing yet")
+    #######
+        #Code goes here
+    #######
 
 def get_config():
     config = {
@@ -22,98 +84,36 @@ def get_config():
 def setup_django():
     get_config()
 
-#Creates account
-def create_account(self):
-    print("Enter user name")
-    user_name = raw_input()
-    print("Enter password")
-    password = raw_input()
-#######
-    #Code goes here
-#######
-
-#Turns automatic syncing on
-def sync(self):
-    config = get_config()
-    setup_django()
-    self.controller.configure(config)
-    self.controller.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        self.controller.__teardown__()
-
-#User command that changes signed on user's password
-def change_pass(self):
-    print("Nothing yet")
-#######
-    #Code goes here
-#######
-
-#Prints user database
-def all_users(self):
-    print("Nothing yet")
-#######
-    #Code goes here
-#######
-
-#Prints files associated with a user
-def print_user_files(self):
-    print("Enter username:")
-    username = raw_input()
-    self.controller.print_user_files(username)
-
-#Removes user
-def remove(self):
-    print("Enter username:")
-    username = raw_input()
-#######
-    #Code goes here
-#######
-
-#Admin command that changes the password of the given user
-def change_user_pass(self):
-    print("Enter username:")
-    username = raw_input()
-
-def history(self):
-    print("Nothing yet")
-#######
-    #Code goes here
-#######
-
 def launch():
+    server = OneDirServer()
     #Starts command line prompt
     print "List of commands: CreateAccount, Sync, ChangePassword, UserInfo, PrintUserFiles, RemoveUser, ChangeUserPassword, History, Exit"
     sys.stdout.flush()
     response = raw_input()
     while (response != "Exit"):
         if (response == "CreateAccount"):
-            create_account()
+            server.create_account()
             response = raw_input()
         elif (response == "Sync"):
-            sync()
+            server.sync(server)
             response = raw_input()
         elif (response == "ChangePassword"):
-            change_pass()
+            server.change_pass()
             response = raw_input()
         elif (response == "UserInfo"):
-            all_users()
+            server.all_users()
             response = raw_input()
         elif (response == "PrintUserFiles"):
-            print_user_files()
+            server.print_user_files()
             response = raw_input()
         elif (response == "RemoveUser"):
-            print "Enter username to remove:"
-            sys.stdout.flush()
-            remove()
+            server.remove()
             response = raw_input()
         elif (response == "ChangeUserPassword"):
-            change_user_pass()
+            server.change_user_pass()
             response = raw_input()
         elif (response == "History"):
-            history()
+            server.history()
             response = raw_input()
         else:
             print "Invalid Command"
