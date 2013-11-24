cc93560143a393ed37c9b1084702b65507bef029
jha5cn <jha5cn@virginia.edu>
2013-11-24 17:44:18 -0500

    Command line

--- TransferTest/OneDirServer.py
+++ TransferTest/OneDirServer.py
@@ -22,6 +22,16 @@ def get_config():
 def setup_django():
     get_config()
 
+#Creates account
+def create_account(self):
+    print("Enter user name")
+    user_name = raw_input()
+    print("Enter password")
+    password = raw_input()
+#######
+    #Code goes here
+#######
+
 #Turns automatic syncing on
 def sync(self):
     config = get_config()
@@ -34,28 +44,78 @@ def sync(self):
     except KeyboardInterrupt:
         self.controller.__teardown__()
 
+#User command that changes signed on user's password
+def change_pass(self):
+#######
+    #Code goes here
+#######
+
+#Prints user database
+def all_users(self):
+#######
+    #Code goes here
+#######
+
 #Prints files associated with a user
-def print_user_files(self, username):
+def print_user_files(self):
+    print("Enter username:")
+    username = raw_input()
     self.controller.print_user_files(username)
 
-#PRINTS USER DATABASE
-#def users_info(self):
-    #print all user info
+#Removes user
+def remove(self):
+    print("Enter username:")
+    username = raw_input()
+#######
+    #Code goes here
+#######
+
+#Admin command that changes the password of the given user
+def change_user_pass(self):
+    print("Enter username:")
+    username = raw_input()
+
+def history(self):
+#######
+    #Code goes here
+#######
 
 def launch():
     #Starts command line prompt
-    print "List of commands: Sync, PrintUserFiles, UserInfo"
+    print "List of commands: CreateAccount, Sync, ChangePassword, UserInfo, PrintUserFiles, RemoveUser, ChangeUserPassword, History, Exit"
     sys.stdout.flush()
     response = raw_input()
-    if (response == "Sync"):
-        sync()
-    elif (response == "PrintUserFiles"):
-        print "Enter username:"
-        sys.stdout.flush()
-        userresponse = raw_input()
-        print_user_files(userresponse)
-#    elif (response == "UserInfo"):
-#        users_info()
+    while (response != "Exit"):
+        if (response == "CreateAccount"):
+            create_account()
+            response = raw_input()
+        elif (response == "Sync"):
+            sync()
+            response = raw_input()
+        elif (response == "ChangePassword"):
+            change_pass()
+            response = raw_input()
+        elif (response == "UserInfo"):
+            all_users()
+            response = raw_input()
+        elif (response == "PrintUserFiles"):
+            print_user_files()
+            response = raw_input()
+        elif (response == "RemoveUser"):
+            print "Enter username to remove:"
+            sys.stdout.flush()
+            remove()
+            response = raw_input()
+        elif (response == "ChangeUserPassword"):
+            change_user_pass()
+            response = raw_input()
+        elif (response == "History"):
+            history()
+            response = raw_input()
+        else:
+            print "Invalid Command"
+            response = raw_input()
+    print("Exited")
 
 if __name__ == "__main__":
     launch()
